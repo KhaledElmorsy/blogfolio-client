2f41bda45f531a96b30bb9863d6461230be51017
KhaledElmorsy <khaled.mg.a@gmail.com>
2024-03-08 15:22:59 +0200

    feat(services): Add query param type checking to axios wrapper

--- src/services/api/util/axiosWithEndpoint.ts
+++ src/services/api/util/axiosWithEndpoint.ts
@@ -1,5 +1,5 @@
 import { EndpointSchema, InferEndpoint } from '@blogfolio/types/Endpoint';
-import axios, { type AxiosResponse } from 'axios';
+import axios, { AxiosRequestConfig, type AxiosResponse } from 'axios';
 
 axios.defaults.validateStatus = function () {
   return true;
@@ -8,11 +8,14 @@ axios.defaults.validateStatus = function () {
 export default async function axiosWithEndpoint<T extends EndpointSchema>(
   method: 'put' | 'get' | 'post' | 'delete',
   url: string,
-  body: InferEndpoint<T>['request']['body'] = {}
+  body: InferEndpoint<T>['request']['body'] = {},
+  config: AxiosRequestConfig & {
+    params?: InferEndpoint<T>['request']['query'];
+  } = {}
 ) {
   type Response = InferEndpoint<T>['response'];
 
-  const response = await axios[method]<Response['body']>(url, body);
+  const response = await axios[method]<Response['body']>(url, body, config);
   const { status, data } = response;
 
   return { status, body: data, axios: response } as Response & {
