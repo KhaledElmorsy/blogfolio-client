612694afa9a74bee4ec7b2c26432b69863beb4e3
KhaledElmorsy <khaled.mg.a@gmail.com>
2024-03-13 12:21:52 +0200

    feat(components): Add basic layout component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { Layout } from './components/Layout';
+import { Routes, Route } from 'react-router-dom';
+import { UserProvider } from './contexts/UserContext';
+import './scss/App.scss';
+
 function App() {
-  return <>Hello World</>;
+  return (
+    <>
+      <UserProvider>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            
+          </Route>
+        </Routes>
+      </UserProvider>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,36 @@
+import { Link, Outlet } from 'react-router-dom';
+import style from './style.module.scss';
+import { useUserContext } from '@/contexts/UserContext';
+
+export function Layout() {
+  const { user } = useUserContext();
+
+  return (
+    <div className={style.pageContainer}>
+      <div className={style.topBar}>
+        <Link to="/" className={style.logo}>
+          Blogfolio
+        </Link>
+        <div className={style.navBar}>
+          <span className={style.links}>
+            <Link to="/">Home</Link>
+            <Link to="/portfolio">Portfolio</Link>
+            <Link to="/blogs">Blogs</Link>
+            <Link to="/account">Account</Link>
+          </span>
+          <span className={style.userActions}>
+            {!user ? (
+              <Link to="/login">Login</Link>
+            ) : (
+              <>
+                <img src={user.photoSmall ?? ''} className={style.userPic} />
+                <Link to="/logout">Logout</Link>
+              </>
+            )}
+          </span>
+        </div>
+      </div>
+      <Outlet />
+    </div>
+  );
+}
