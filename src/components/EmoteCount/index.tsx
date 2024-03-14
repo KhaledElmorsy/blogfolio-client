35a7d9ba817ced3b1cf97ba1515fbbe7323e9c0a
KhaledElmorsy <khaled.mg.a@gmail.com>
2024-03-15 01:17:26 +0200

    feat(components): Add emote count component

---
+++ src/components/EmoteCount/index.tsx
@@ -0,0 +1,25 @@
+import { useEmoteContext } from '@/contexts/EmoteContext';
+import type { EmoteCount } from '../PostList';
+import style from './style.module.scss';
+
+interface EmoteCountProps {
+  emoteCounts: EmoteCount[];
+}
+
+export function EmoteCount({ emoteCounts }: EmoteCountProps) {
+  const emoteMap = useEmoteContext();
+
+  return (
+    <div className={style.emoteContainer}>
+      {!emoteCounts.length
+        ? null
+        : emoteCounts
+            .sort((a, b) => a.emoteID - b.emoteID)
+            .map((emote) => (
+              <span key={emote.emoteID}>
+                {emoteMap[emote.emoteID]} {emote.count}
+              </span>
+            ))}
+    </div>
+  );
+}
