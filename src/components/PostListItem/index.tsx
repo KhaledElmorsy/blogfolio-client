0a8e52168df3a1df6961f1b96c3e216a8c277923
KhaledElmorsy <khaled.mg.a@gmail.com>
2024-03-15 01:20:11 +0200

    feat(components): Add Post list item component

---
+++ src/components/PostListItem/index.tsx
@@ -0,0 +1,59 @@
+import type { EmoteCount as EmoteCountType, LightPost } from '../PostList';
+import { useEffect, useState } from 'react';
+import { getUserData } from '@/services/api/users';
+import { SuccessCode } from '@blogfolio/types/Response';
+import { Resources } from '@blogfolio/types/User';
+import style from './style.module.scss';
+import { Spinner } from '../Spinner';
+import { EmoteCount } from '../EmoteCount';
+
+type User = Resources['QueriedUser'];
+
+interface PostListItemProps extends LightPost {
+  emotes: EmoteCountType[];
+}
+
+export function PostListItem({
+  title,
+  summary,
+  userID,
+  emotes,
+  createdAt,
+}: PostListItemProps) {
+  const [user, setUser] = useState<User>();
+  useEffect(() => {
+    async function updateUserData() {
+      const response = await getUserData(userID);
+      if (response.status !== SuccessCode.Ok) {
+        throw new Error("Something went wrong getting a post author's data.");
+      }
+      setUser(response.body.user);
+    }
+
+    updateUserData().catch(console.error);
+  }, []);
+
+  const createdAtDate = new Date(createdAt);
+  const renderedDate = `${createdAtDate.getMonth()}/${createdAtDate.getFullYear()}`;
+
+  return !user ? (
+    <Spinner />
+  ) : (
+    <div className={style.itemContainer}>
+      <div className={style.userData}>
+        <img className={style.userPFP} src={user?.photoSmall ?? ''} />
+        <p className={style.username}>{user.username}</p>
+      </div>
+      <div className={style.postData}>
+        <h4 className={style.title}>{title}</h4>
+        <p className={style.summary}>{summary ?? ''}</p>
+        <div className={style.miscContainer}>
+          <p className={style.date}>{renderedDate}</p>
+          <div className={style.emotes}>
+            <EmoteCount emoteCounts={emotes} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
